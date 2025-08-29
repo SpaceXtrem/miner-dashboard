d8172c7e80be541f288efd329e1821bf85f2be53
SpaceXtrem <Robinson8@orange.fr>
2025-08-29 18:07:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+app = FastAPI()
+
+# Dossier pour fichiers CSS, JS, images
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Dossier pour templates HTML
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
